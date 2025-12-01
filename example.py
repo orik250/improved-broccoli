b558daccbb19d69bc5cfc08dca929505dba46212
orik250 <orik5@nycstudents.net>
2025-12-01 02:38:52 -0500

    Add files via upload

---
+++ example.py
@@ -0,0 +1,4 @@
+name = input("What's your name? ")
+age = int(input("How old are you? "))
+print("Nice to meet you, " + name+"!")
+print(int("if you are "+str(age)+" years old that means you were born in ")+str((2025-age)))
